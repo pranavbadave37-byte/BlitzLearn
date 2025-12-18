321cbea93b05250a5ef45b65b39d919786cfdf9d
Vedantsd <vedantdesai767@gmail.com>
2025-12-18 22:53:52 +0530

    Minor design changes

--- static/js/dashboard-script.js
+++ static/js/dashboard-script.js
@@ -11,13 +11,13 @@ function toggleTheme() {
 }
 
 function updateThemeIcon() {
-    const icon = document.getElementById('theme-icon');
+    const iconPath = document.getElementById('switch-icon');
     const isDark = document.body.classList.contains('dark');
     
     if (isDark) {
-        icon.innerHTML = '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>';
+        iconPath.setAttribute('d', 'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z');
     } else {
-        icon.innerHTML = '<path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>';
+        iconPath.setAttribute('d', 'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z');
     }
 }
 
